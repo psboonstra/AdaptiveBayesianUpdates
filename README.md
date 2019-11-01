3358aeb3d577a1e114c7873eae45d5dbc5d59f23
Phil Boonstra <pbgamer@rocketmail.com>
2019-10-31 22:03:58 -0400

    replaced exemplar.r with vignette.r; updated readme to reflect; added vignette.pdf

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 # Adaptive Bayesian Updates
 
-This is a companion repository for the `R` package `adaptBayes` available here: https://github.com/umich-biostatistics/adaptBayes
+This is a companion repository for the `R` package `adaptBayes` available here: https://github.com/umich-biostatistics/adaptBayes. This package contains all of the additional code needed for conducting the simulation study in Boonstra and Barbaro (2018)
 
-First, install and load that package using the following commands:
+Install and load the `adaptBayes` package using the following commands:
 
 ```r
-library(devtools)
-# may take some time:
-install_github('umich-biostatistics/adaptBayes') 
-
-library(adaptBayes)
+if(!require(adaptBayes)) {
+  library(devtools)
+  # if installation is necessary, compiling everything will take a few minutes
+  install_github('umich-biostatistics/adaptBayes') 
+} 
 ```
 
-Second, use the scripts in this repository to reproduce 
-the simulations studies in Boonstra and Barbaro
+After installing the package, you can use the scripts in this repository to reproduce the simulations studies in the manuscript. See also `vignette.pdf` for a vignette describing the typical usage of the adaptive Bayesian priors on a simulated dataset
 
 ### Further details
 
-In more detail, there are six files included in this repository (in addition to this README): one text file (ending in <samp>.txt</samp>) and five <samp>R</samp> scripts (ending in  <samp>.R</samp>). The simulation studies reported in Boonstra and Barbaro were run using commit 22
+In more detail, there are six files included in this repository (in addition to this README and `vignette.pdf`): one text file (<samp>run_abu_sims.txt</samp>) and five <samp>R</samp> scripts (ending in  <samp>.R</samp>). The simulation studies reported in Boonstra and Barbaro were run using commit 22
 
 #### Text file
 <samp>run_abu_sims.txt</samp> is the script for submitting parallel runs of <samp>run_aub_sims.R</samp> (described below) to a cluster that is running SLURM. The following command run in terminal will do this:
@@ -28,7 +27,7 @@ The script assumes that you are working in your home directory. Edit the script 
 
 #### <samp>R</samp> files
 
-<samp>exemplar.R</samp> creates a single simulated dataset and walks through analyzing these data using the various adaptive priors
+<samp>vignette.R</samp> creates a single simulated dataset and walks through analyzing these data using the various adaptive priors. It can also be `knit` to create a copy of `vignette.pdf`
 
 <samp>run_abu_sims.R</samp> is the script to conduct the large-scale simulation study described in the manuscript. On a local machine, the user may choose a specific <samp>array_id</samp> (as described in this script's documentation) and run the code locally on his/her machine. On a cluster running SLURM, the user can use this script to submit multiple jobs simultaneously (as described in the description of
 <samp>run_abu_sims.txt</samp> above). 
@@ -41,8 +40,7 @@ The script assumes that you are working in your home directory. Edit the script 
 
 #### Current Suggested Citation
 
-Boonstra, Philip S. and Barbaro, Ryan P., "Incorporating Historical Models
-with Adaptive Bayesian Updates" (2018) *Biostatistics* 
+Boonstra, Philip S. and Barbaro, Ryan P., "Incorporating Historical Models with Adaptive Bayesian Updates" (2018) *Biostatistics* 
 https://doi.org/10.1093/biostatistics/kxy053
 
 <a href="https://biostats.bepress.com/umichbiostat/paper124">Authors' Copy </a>
--- vignette.R
+++ vignette.R
@@ -1,8 +1,36 @@
-#DESCRIPTION: Example to demonstrate the R functions. 
+#' ---
+#' title: "*Vignette* for `adaptBayes`"
+#' author: "Philip S. Boonstra"
+#' date: "October 31, 2019"
+#' geometry: margin=2cm
+#' output: 
+#'  pdf_document: 
+#'    toc: no
+#' header-includes:
+#'  - \usepackage{booktabs}
+#'  - \usepackage{hyperref}
+#'  - \usepackage{float}
+#'  - \usepackage{caption}
+#'  - \floatplacement{figure}{H}
+#' ---
+
+#+ echo=F, warning=F, message=F, cache = T, include = F
+options(digits = 3)
+
+#' ## Introduction
+#' 
+#' This vignette presents a step-by-step approach for using the `R` 
+#' functions `glm_standard()`, `glm_nab()`, and `glm_sab()` in the
+#' `adaptBayes` package. 
+#' 
+#' First, install and load the `adaptBayes` package and other necessary 
+#' packages:
+
+#+ echo=T, warning=F, message=F, cache = T, include = T
 
 if(!require(adaptBayes)) {
   library(devtools)
-  # may take some time:
+  # if installation is necessary, compiling everything will take a few minutes
   install_github('umich-biostatistics/adaptBayes') 
 } 
 
@@ -10,20 +38,36 @@ library(mice);library(Hmisc);library(MASS);
 library(rstan);library(Matrix);library(mnormt);
 library(tidyverse);
 
+# some recommended options from the STAN development team
 options(mc.cores = parallel::detectCores());
 rstan_options(auto_write = TRUE);
 
-source("functions_simulation.R"); # For draw_data() function
+#' Also, source the script that contains the data-simulating function 
+#' `draw_data()` and the function `solve_for_hiershrink_scale()`, which
+#' is used to solve for the scale parameter. 
 
-## Draw Data ====##########################################################################
-#Choose your own values
+#+ echo=T, warning=F, message=F, cache = T, include = T
+
+source("functions_simulation.R"); # For access to the draw_data() function
+
+
+#+ echo=T, warning=F, message=F, cache = T, include = T
+
+#' ## Draw data
+
+## Draw Data ====##############################################
+# Choose your own values if desired
 set.seed(1);
 n_hist = 500;
 n_curr = 100;
 n_new = 1e3;
+# this is different from the misspecified marginal prevalence;
+# see Remark 3 in the manuscript
 true_mu_hist = 0;
-true_mu_curr = -2.5;#Note the difference in baseline prevalence
+true_mu_curr = -2.5;
+# original betas common to both analyses:
 true_betas_orig = c(2,-2,1,-1);
+# augmented betas exclusive to current analysis:
 true_betas_aug = c(-1,-1,0.5,0.5,-0.25,-0.25);
 covariate_args = list(x_correlation = 0.2,
                       x_orig_binom = 1:length(true_betas_orig),
@@ -45,11 +89,33 @@ x_curr_orig = as.matrix(complete_dat$x_curr_orig);
 colnames(x_hist_orig) = 
   colnames(x_curr_orig) = orig_covariates;
 x_curr_aug = as.matrix(complete_dat$x_curr_aug);
+# 'x_hist_aug' is essentially missing here, since those data
+# were not collected in the historical analysis
 colnames(x_curr_aug) = aug_covariates;
 p = length(true_betas_orig);
 q = length(true_betas_aug);
 
-##Methods to fit====##########################################################################
+#+ echo=T, warning=F, message=F, cache = T, include = T
+
+#' ## Methods to fit
+#' 
+#' 'Historical' is a horseshoe prior applied only to the historical 
+#' data. It is a method in and of itself as well as
+#' the prior analysis that will be provided to the NAB / SAB methods.
+#' 
+#' 'Standard' is a horseshoe prior applied to the current data. It is
+#' presumably what would be done in the absence of any knowledge about
+#' the historical analysis. 
+#' 
+#' 'NAB' and 'SAB' are the naive and sensible adaptive Bayesian priors, 
+#' respectively. 
+#' 
+#' Here the hyperparameters for $\phi$ are also described. The truncation
+#' to the interval [0,1] is always assumed and not necessary to specify. 
+
+##Methods to fit====#########################################
+# Each element of this list will be crossed with each adaptive
+# prior. 
 phi_params = list("Agnostic" = c(mean = 0.5, sd = 2.5),
                   "Optimist" = c(mean = 1, sd = 0.25));
 
@@ -62,23 +128,33 @@ expanded_meth_names = c("Historical",
                         paste0("NAB",names(phi_params)),
                         paste0("SAB",names(phi_params)));
 
-## Model Params====##########################################################################
+#+ echo=T, warning=F, message=F, cache = T, include = T
+
+#' ## Model hyperparameters
+#' 
+#' Specify the hyperparameters, including deriving values of $c$ using 
+#' the function `solve_for_hiershrink_scale`
+
+## Model hyperparameters====################
 local_dof = 1;
 global_dof = 1;
-slab_precision = (1/15)^2;
-nab_augmented_scale = 0.05;
-sab_imputes_list = list(c(1,100));
+slab_precision = (1/15)^2; # 'd' in the paper
+nab_augmented_scale = 0.05;# 'tilde_c' in the paper
+sab_imputes_list = list(c(1,100)); # Section S1 supplement
 stopifnot(class(sab_imputes_list) == "list");
 sab_num_imputes_each = unlist(lapply(sab_imputes_list,diff)) + 1;
 max_sab_index = max(unlist(lapply(sab_imputes_list,max)));
 min_sab_index = min(unlist(lapply(sab_imputes_list,min)));
 store_hierarchical_scales = 
-  prior_eff = #prior effective number of original parameters = mean(rowSums(1-kappa[orig]))
+  #prior effective number of original parameters = mean(rowSums(1-kappa[orig]))
+  prior_eff =
   vector("list",length(base_meth_names));
-names(store_hierarchical_scales) = names(prior_eff) = base_meth_names;
+names(store_hierarchical_scales) = 
+  names(prior_eff) = 
+  base_meth_names;
 power_prop_nonzero_prior = 1/3;
-#Historical: access to historical data alone, with a skeptical prior. This is both a method in and of itself as well as
-#the 'prior' analysis that will be provided to the SAB methods. 
+
+# 'c' for Historical
 foo = solve_for_hiershrink_scale(target_mean1 = -0.5 + p ^ power_prop_nonzero_prior,
                                  target_mean2 = NA,
                                  npar1 = p, 
@@ -93,9 +169,8 @@ foo = solve_for_hiershrink_scale(target_mean1 = -0.5 + p ^ power_prop_nonzero_pr
 store_hierarchical_scales$Historical = foo$scale1;
 prior_eff$Historical = foo$prior_num1;
 rm(foo);
-#Standard: access to current data alone, with a skeptical prior. 
-#This also corresponds to the standard scales for SAB and NAB, where standard means that
-#no historical information is used
+
+# 'c' for the Standard, NAB, and SAB models
 foo = solve_for_hiershrink_scale(target_mean1 = -0.5 + (p + q) ^ power_prop_nonzero_prior,
                                  target_mean2 = NA,
                                  npar1 = p + q, 
@@ -119,26 +194,44 @@ rm(foo);
 #
 store_hierarchical_scales$NAB_aug_tilde = nab_augmented_scale;
 
-## MC Params ====##########################################################################
-mc_warmup = 2e3;
+#+ echo=T, warning=F, message=F, cache = T, include = T
+
+#' ## Model hyperparameters
+
+## MC Params ====##########################################
+mc_warmup = 1e3;
 mc_iter_after_warmup = 1e3;
-only_prior = 0;
+only_prior = 0;# set to 1 if you only want to sample from prior
 mc_chains = 2;
 mc_thin = 1;
 mc_stepsize = 0.1;
 mc_adapt_delta_relaxed = 0.99;
-mc_adapt_delta_strict = 0.9999;
-mc_max_treedepth = 19;
+mc_adapt_delta_strict = 0.999;
+mc_max_treedepth = 15;
 ntries_per_iter = 2;
 
+#+ echo=T, warning=F, message=F, cache = T, include = T
 
-## Historical ====##########################################################################
+#' ## Methods: Historical
+#' 
+
+
+## Historical ====#########################################
 #Historical analysis only
 curr_method = "Historical";
 y = y_hist;
 x_standardized = x_hist_orig;
 beta_orig_scale = store_hierarchical_scales[[curr_method]];
 beta_aug_scale = store_hierarchical_scales[[curr_method]];
+
+#' The values `p` and `q` should add up to be equal to the number
+#' of columns in `x_standardized`. It is assumed that the first 
+#' `p` columns correspond to the original covariates, and the 
+#' second `q` columns correspond to the augmented covariates. 
+#' For `glm_standard`, the only difference is that you can specify
+#' different scale hyperparameters to be applied to the original
+#' and augmented regression coefficients. 
+#' 
 
 foo = glm_standard(y = y,
                    x_standardized = x_standardized, 
@@ -170,13 +263,24 @@ names(foo);
 #Garbage cleanup
 rm(foo, curr_method, y, x_standardized, beta_orig_scale, beta_aug_scale);
 
-## Standard ====##########################################################################
+#+ echo=T, warning=F, message=F, cache = T, include = T
+
+#' ## Methods: Standard
+#' 
+
+## Standard ====#############################################
 #Standard analysis of current data, ignoring historical model
 curr_method = "Standard";
 y = y_curr;
 x_standardized = cbind(x_curr_orig,x_curr_aug);
 beta_orig_scale = store_hierarchical_scales[[curr_method]];
 beta_aug_scale = store_hierarchical_scales[[curr_method]];
+
+#' As in the previous model, the values `p` and `q` need to add 
+#' up to be equal to the number of columns in `x_standardized`. 
+#' But note now that `x_standardized` has more columns and contains
+#' a different set of observations.
+#' 
 
 foo = glm_standard(y = y,
                    x_standardized = x_standardized, 
@@ -208,7 +312,12 @@ names(foo);
 #Garbage cleanup
 rm(foo, curr_method, y, x_standardized, beta_orig_scale, beta_aug_scale);
 
-## NAB ====##########################################################################
+#+ echo=T, warning=F, message=F, cache = T, include = T
+
+#' ## Methods: NAB
+#' 
+
+## NAB ====#########################################################
 #Naive Adaptive Bayes: apply Historical analysis directly as a prior on beta_orig. 
 
 curr_base_method = "NAB";
@@ -269,7 +378,12 @@ for(prior_type in names(phi_params)) {
 
 rm(foo, curr_method, y, x_standardized, beta_orig_scale, beta_aug_scale, beta_aug_scale_tilde, prior_type);
 
-## SAB ====##########################################################################
+#+ echo=T, warning=F, message=F, cache = T, include = T
+
+#' ## Methods: SAB
+#' 
+
+## SAB ====######################################################
 #Sensible Adaptive Bayes: apply Historical analysis as a prior on beta_orig + projection%*%beta_aug 
 curr_base_method = "SAB";
 y = y_curr;
@@ -277,6 +391,8 @@ x_standardized = cbind(x_curr_orig,x_curr_aug);
 beta_orig_scale = store_hierarchical_scales[[curr_base_method]];
 beta_aug_scale = store_hierarchical_scales[[curr_base_method]];
 
+#' This function creates the projection matrix P in Equation (3.8)
+#' of the manuscript
 aug_projection = create_projection(x_curr_orig = x_curr_orig,
                                    x_curr_aug = x_curr_aug,
                                    eigenvec_hist_var = t(eigendecomp_hist_var$vectors),
@@ -326,24 +442,33 @@ for(prior_type in names(phi_params)) {
 rm(foo, curr_method, y, x_standardized, beta_orig_scale, beta_aug_scale, prior_type);
 rm(aug_projection, alpha_prior_mean, alpha_prior_cov, scale_to_variance225, eigendecomp_hist_var);
 
+#+ echo=F, warning=F, message=F, cache = T, include = F
+options(digits = 3)
 
-## Results ====##########################################################################
+#+ echo=T, warning=F, message=F, cache = T, include = T
 
-#Mean
-round(colMeans(beta_Standard),3);
-round(colMeans(beta_NABAgnostic),3);
-round(colMeans(beta_NABOptimist),3);
-round(colMeans(beta_SABAgnostic),3);
-round(colMeans(beta_SABOptimist),3);
-#true values
+#' ## Results
+#' 
+
+## Results ====####################################################
+
+# Posterior mean
+colMeans(beta_Standard);
+colMeans(beta_NABAgnostic);
+colMeans(beta_NABOptimist);
+colMeans(beta_SABAgnostic);
+colMeans(beta_SABOptimist);
+# Compared to true values
 c(true_betas_orig,true_betas_aug);
-#Standard deviation
-round(apply(beta_Standard,2,sd),3);
-round(apply(beta_NABAgnostic,2,sd),3);
-round(apply(beta_NABOptimist,2,sd),3);
-round(apply(beta_SABAgnostic,2,sd),3);
-round(apply(beta_SABOptimist,2,sd),3);
-#RMSE
+
+# Posterior standard deviation
+apply(beta_Standard,2,sd);
+apply(beta_NABAgnostic,2,sd);
+apply(beta_NABOptimist,2,sd);
+apply(beta_SABAgnostic,2,sd);
+apply(beta_SABOptimist,2,sd);
+
+# Root mean-squared error
 matrix_true_beta = matrix(c(true_betas_orig,true_betas_aug),
                           nrow = mc_iter_after_warmup * mc_chains,
                           ncol = p + q,
@@ -353,3 +478,5 @@ sqrt(mean(rowSums((beta_NABAgnostic - matrix_true_beta)^2)));
 sqrt(mean(rowSums((beta_NABOptimist - matrix_true_beta)^2)));
 sqrt(mean(rowSums((beta_SABAgnostic - matrix_true_beta)^2)));
 sqrt(mean(rowSums((beta_SABOptimist - matrix_true_beta)^2)));
+
+#' 
